f0304194fd529617da9812a5cd4a05fab8a773a2
BeyramTaglietti <beyram.taglietti@gmail.com>
2025-11-05 16:14:05 +0100

    feat: integrate TanStack Router and update dependencies in web app

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// import "@/index.css";
+import { routeTree } from "@/routeTree.gen";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+// Create a new router instance
+const router = createRouter({
+  routeTree,
+  defaultPreload: "intent",
+  context: null!,
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+    <ReactQueryDevtools initialIsOpen={false} />
+  </StrictMode>
+);
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,13 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import tanstackRouter from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tanstackRouter({ target: "react", autoCodeSplitting: true }),
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: ["babel-plugin-react-compiler"],
       },
     }),
   ],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
